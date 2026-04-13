c2e8aad7e1eb18f94e5e65c03970d65ee3ddfe03
Dhilip <dhiliban.m@payhuddle.com>
2026-04-13 18:43:11 +0530

    additional- feature -added

--- Header.js
+++ Header.js
@@ -4,3 +4,9 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     console.log("Header")
 }
+
+//update in new-feature and additional
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
